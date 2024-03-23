b19436b03a1c2b41e44dad01f2c02b0b2d706462
akash-soni <akash.200287@gmail.com>
2024-03-23 11:43:52 +0530

    model downloaded and copied

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'akash',
+    author_email= 'akash.200287@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
